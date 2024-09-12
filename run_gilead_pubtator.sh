93d8988ab72d1131983dbc3bc7dd5908750c0707
Ishaan Bhatnagar <ishaanbhatnagar@Ishaans-MacBook-Pro.local>
2024-09-12 17:34:33 +0530

    Updated shell util or io paths

--- run_gilead_pubtator.sh
+++ run_gilead_pubtator.sh
@@ -14,10 +14,24 @@ else
     exit 1
 fi
 
-####### Run AIONER
+####### Run the AIONER container
 
-# Run AIONER Pipeline with Bioformer-Softmax
-python AIONER/src/AIONER_Run.py -i ../../data/bioc_xml/ -m AIONER/pretrained_models/AIONER/Bioformer-softmax-AIONER.h5 -v ../vocab/AIO_label.vocab -e ALL -o ../../data/bioformer_annotated
+# Run the container
+docker run -it \
+  --name my-aioner-container \
+  -v $(pwd)/data/bioc_xml:/app/example/input \
+  -v $(pwd)/data/bioformer_annotated:/app/example/output/bioformer_annotated \
+  -v $(pwd)/data/pubmedbert_annotated:/app/example/output/pubmedbert_annotated \
+  my-aioner
+
+# Run AIONER Pipeline with Bioformer-Softmax in Container
+docker exec -it my-aioner-container \
+python AIONER/src/AIONER_Run.py \
+  -i ../example/input/ \
+  -m ../pretrained_models/AIONER/Bioformer-softmax-AIONER.h5 \
+  -v ../vocab/AIO_label.vocab \
+  -e ALL \
+  -o ../example/output/bioformer_annotated
 # Check if Pipeline executed successfully
 if [ $? -eq 0 ]; then
     echo "AIONER Pipeline with Bioformer-Softmax executed successfully."
@@ -27,8 +41,14 @@ else
 fi
 
 
-# Run AIONER Pipeline with PubmedBERT-CRF
-python AIONER/src/AIONER_Run.py -i ../../data/bioc_xml/ -m AIONER/pretrained_models/AIONER/PubmedBERT-CRF-AIONER.h5 -v ../vocab/AIO_label.vocab -e ALL -o ../../data/pubmedbert_annotated
+# Run AIONER Pipeline with PubmedBERT-CRF in Container
+docker exec -it my-aioner-container \
+python AIONER/src/AIONER_Run.py \
+  -i ../example/input/ \
+  -m ../pretrained_models/AIONER/PubmedBERT-CRF-AIONER.h5 \
+  -v ../vocab/AIO_label.vocab \
+  -e ALL \
+  -o ../example/output/pubmedbert_annotated
 # Check if Pipeline executed successfully
 if [ $? -eq 0 ]; then
     echo "AIONER Pipeline with PubmedBERT-CRF executed successfully."
@@ -39,8 +59,18 @@ fi
 
 ######### Run GNorm2
 
+# Run the container
+docker run -it \
+  --name my-gnorm2-container \
+  -v $(pwd)/data/bioformer_annotated:/app/input/bioformer_annotated \
+  -v $(pwd)/data/pubmedbert_annotated:/app/input/pubmedbert_annotated \
+  -v $(pwd)/data/bioformer_annotated_normalized:/app/output/bioformer_annotated_normalized \
+  -v $(pwd)/data/pubmedbert_annotated_normalized:/app/output/pubmedbert_annotated_normalized \
+  my-gnorm2
+
 # Run Gnorm2 Pipeline on AIONER Output
-sh Gnorm2/Gnorm2.sh data/bioformer_annotated data/bioformer_annotated_normalized
+docker exec -it my-gnorm2-container \
+sh ./Gnorm2.sh input/bioformer_annotated output/bioformer_annotated_normalized
 # Check if Pipeline executed successfully
 if [ $? -eq 0 ]; then
     echo "GNorm2 Pipeline with Bioformer-Softmax executed successfully."
@@ -49,7 +79,8 @@ else
     exit 1
 fi
 
-sh Gnorm2/Gnorm2.sh data/pubmedbert_annotated data/pubmedbert_annotated_normalized
+docker exec -it my-gnorm2-container \
+sh ./Gnorm2.sh input/pubmedbert_annotated output/pubmedbert_annotated_normalized
 if [ $? -eq 0 ]; then
     echo "GNorm2 Pipeline with PubmedBERT-CRF executed successfully."
 else
@@ -60,4 +91,4 @@ fi
 ######### Run Data Migration Util to copy processed files to S3
 
 
-######### Run the Data Processing Pipeline - Chunking + Indexing
+######### Run the Data Processing Pipeline - Chunking + Embedding + Indexing
