37095a072da2acc4bd30d7b3206ee91e9e5d7539
Ishaan Bhatnagar <ishaanbhatnagar@Ishaans-MacBook-Pro.local>
2024-10-16 11:35:32 +0530

    Notebooks for embeddings analysis

--- src/data_processing/indexing/index_handler.py
+++ src/data_processing/indexing/index_handler.py
@@ -45,9 +45,9 @@ if __name__ == "__main__":
     llm_model = "BedrockClaude"
 
     chunk_strategies = [
-        "grouped_annotation_aware_sliding_window_prepend_bioformer",
+        # "grouped_annotation_aware_sliding_window_prepend_bioformer",
         # "grouped_annotation_aware_sliding_window_inline_bioformer",
-        # "grouped_annotation_aware_sliding_window_append_bioformer",
+        "grouped_annotation_aware_sliding_window_append_bioformer",
         # "grouped_annotation_aware_sliding_window_prepend_pubmedbert",
         # "grouped_annotation_aware_sliding_window_inline_pubmedbert",
         # "grouped_annotation_aware_sliding_window_append_pubmedbert",
@@ -63,12 +63,21 @@ if __name__ == "__main__":
     # chunk_strategy = 'sliding_window_append_pubmedbert'
     # chunk_strategy = 'sliding_window_inline_pubmedbert'
 
+    articles_to_process = [
+        "PMC_7614604",
+        # "PMC_9911803"
+    ]
+
     for chunk_strategy in chunk_strategies:
         cnt = 0
         print(chunk_strategy)
         for cur_vector_db in vector_db_list:
             for cur_file in os.listdir("../../../data/chunks_11_oct"):
-                if cur_file.endswith(".json") and cur_file.startswith(chunk_strategy):
+                if (
+                    cur_file.endswith(".json")
+                    and cur_file.startswith(chunk_strategy)
+                    and articles_to_process[0] in cur_file
+                ):
                     cnt += 1
                     print(f"cur_file: {cur_file}, cnt: {cnt}")
                     logger.info(f"Processing {cur_file}")
