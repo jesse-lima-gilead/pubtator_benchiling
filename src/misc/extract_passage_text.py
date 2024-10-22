4245b73ffb8d5010bb0a2c4ec2a50250607c9214
Ishaan Bhatnagar <ishaanbhatnagar@Ishaans-MacBook-Pro.local>
2024-10-22 10:34:55 +0530

    Senstivity Check notebook

---
+++ src/misc/extract_passage_text.py
@@ -0,0 +1,16 @@
+import json
+
+def extract_text(file_path):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+        passages = ""
+        for chunk in data:
+            passages += chunk["chunk_text"].strip() + "\n\n"
+        return passages
+
+if __name__ == "__main__":
+    file_path = "../../data/chunks_11_oct/passage_prepend_bioformer_PMC_7614604.json"
+    passages = extract_text(file_path)
+    output_file = "../../data/PMC_7614604_chunks/PMC_7614604_passages.txt"
+    with open(output_file, "w") as file:
+        file.write(passages)
