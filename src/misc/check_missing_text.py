89a4778ea6ef5bb501ced50778d2d79a53fb1fb0
Ishaan Bhatnagar <ishaanbhatnagar@Ishaans-MacBook-Pro.local>
2025-01-20 16:43:06 +0530

    Refactored the missing text

--- src/misc/check_missing_text.py
+++ src/misc/check_missing_text.py
@@ -13,34 +13,51 @@ def count_empty_text_tags_in_xml(xml_content):
 
     # Find all <text> tags and check if they are empty
     count = 0
+    total_count = 0
     for text_element in tree.findall(".//text"):
+        total_count += 1
         if not text_element.text or text_element.text.strip() == "":
             count += 1
-    return count
+    return count, total_count
 
 
 def count_empty_text_tags_in_dir(directory):
     """
     Counts the occurrences of empty <text></text> tags in all XML files in a directory.
     """
-    total_count = 0
+    overall_text_tag_total_count = 0
+    overall_empty_text_tag_count = 0
     for filename in os.listdir(directory):
         if filename.endswith(".xml"):
             file_path = os.path.join(directory, filename)
             with open(file_path, "r", encoding="utf-8") as file:
                 xml_content = file.read()
-                count = count_empty_text_tags_in_xml(xml_content)
-                print(f"{filename}: {count} empty <text></text> tags")
-                total_count += count
+                (
+                    empty_text_tag_count,
+                    total_text_tags_count,
+                ) = count_empty_text_tags_in_xml(xml_content)
+                print(
+                    f"{filename}: Total {total_text_tags_count} <text> tags and {empty_text_tag_count} empty <text></text> tags"
+                    f"\tPercent Empty tags ({empty_text_tag_count / total_text_tags_count * 100:.2f}%)"
+                )
+                overall_empty_text_tag_count += empty_text_tag_count
+                overall_text_tag_total_count += total_text_tags_count
 
-    print(f"Total empty <text></text> tags in all files: {total_count}")
+    print(
+        f"\nTotal Text tags {overall_text_tag_total_count}"
+        f"\nTotal empty <text></text> tags in all files: {overall_empty_text_tag_count}"
+        f"\nPercent Empty tags ({overall_empty_text_tag_count / overall_text_tag_total_count * 100:.2f}%)"
+    )
 
 
 if __name__ == "__main__":
     aioner_annotated_path = (
-        "../../data/gilead_pubtator_results/aioner_annotated/bioformer_annotated/"
+        "../../data/enhanced_golden_dataset/ner_processed/aioner_annotated/"
+        # "/Users/ishaanbhatnagar/Downloads/data/golden_dataset/ner_processed/aioner_annotated/"
+        # "/Users/ishaanbhatnagar/Downloads/37_extra_aioner2/"
     )
     gnorm2_annotated_path = (
-        "../../data/gilead_pubtator_results/gnorm2_annotated/bioformer_annotated/"
+        "../../data/enhanced_golden_dataset/ner_processed/gnorm2_annotated/"
+        # "/Users/ishaanbhatnagar/Downloads/data/golden_dataset/ner_processed/gnorm2_annotated/"
     )
-    count_empty_text_tags_in_dir(gnorm2_annotated_path)
+    count_empty_text_tags_in_dir(aioner_annotated_path)
